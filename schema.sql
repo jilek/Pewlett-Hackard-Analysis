f7991b49cf4c7774329d226ee2281b8538996002
jilek <bjilek1@yahoo.com>
2021-04-13 11:02:32 -0500

    updated tables and queries

--- schema.sql
+++ schema.sql
@@ -16,12 +16,12 @@ CREATE TABLE employees (
 );
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
-    from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
+	emp_no INT NOT NULL,
+	from_date DATE NOT NULL,
+	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (emp_no, dept_no)
+	PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE salaries (
 	emp_no INT NOT NULL,
@@ -48,4 +48,5 @@ CREATE TABLE titles (
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
+
 
